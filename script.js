d203c426cf3f533d4cf67794ece7b0be854a161b
Maksim Petrukhin <onlyhuman@mail.ru>
2022-03-25 08:50:52 +0300

    init: start palladiumlab-task

---
+++ script.js
@@ -0,0 +1,25 @@
+const defaultBackgroundUrl = 'image/main/main.png';
+const main = document.querySelector('.main');
+const mainTitle = document.querySelector('.main__title');
+const services = document.querySelector('.services');
+const tabs = document.querySelectorAll('.main__link');
+
+for (let tab of tabs) {
+	tab.addEventListener('mouseenter', function (event) {
+		const name = event.target.dataset.name;
+
+		services.style.backgroundImage = `url(image/main/background/${name}.png)`;
+		mainTitle.classList.add(name);
+		services.classList.add('open');
+		main.classList.add('open');
+	});
+
+	tab.addEventListener('mouseleave', function () {
+		mainTitle.classList.remove('fixes', 'types', 'design', 'renovation');
+		services.style.backgroundImage = '';
+		services.classList.remove('open');
+		main.classList.remove('open');
+	});
+}
+
+
