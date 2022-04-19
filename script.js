bd7f3ae030219b4c07dc02e559a6fdd3e75e1d1e
Max <onlyhuman@mail.ru>
2022-04-19 13:03:36 +0300

    Showing and hiding pop-up

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
+const allButtons = document.querySelectorAll('.nav-burger__icon');
+const jsButtons = document.querySelectorAll('.js-button');
+
+const hidePanel = (event) => {
+	allButtons.forEach((button) => {
+		if (event.currentTarget !== button) {
+			button.classList.remove('js-open');
+			button.nextElementSibling.classList.remove('js-appearance');
+		};
+	});
+}
+
+function appearance (activeButton, activeMenu) {
+	activeButton.addEventListener('click', (event) => {
+
+		hidePanel(event);
+
+		activeButton.classList.toggle('js-open');
+		activeMenu.classList.toggle('js-appearance');
+	});
+};
+
+jsButtons.forEach((button) => {
+	appearance(button, button.nextElementSibling);
+});
+
 const defaultBackgroundUrl = 'image/main/main.png';
 const main = document.querySelector('.main');
 const mainTitle = document.querySelector('.main__title');
 const services = document.querySelector('.services');
 const tabs = document.querySelectorAll('.services__link');
-const mainIcon = document.querySelector('.main__icon')
+const mainIcon = document.querySelector('.main__icon');
+const allPanel = document.querySelectorAll('.js-panel');
 
-const hide = function () {
+
+const hideServices = function () {
 	mainTitle.classList.remove('fixes', 'types', 'design', 'renovation');
 	services.style.backgroundImage = '';
 	services.classList.remove('open');
 	main.classList.remove('open');
 	mainIcon.style.display = 'initial';
-}
 
+	allPanel.forEach((panel) => {
+		panel.style.visibility = 'visible';
+	});
+};
 
 for (let tab of tabs) {
 
@@ -25,39 +56,13 @@ for (let tab of tabs) {
 		services.classList.add('open');
 		main.classList.add('open');
 		mainIcon.style.display = 'none';
-	});
 
-	tab.addEventListener('mouseleave', hide);
-	//link.addEventListener('click', hide);
-};
-
-
-
-
-const buttonMap = document.querySelector('.nav-burger__icon.map');
-const panelMap = document.querySelector('.nav-burger__panel-map');
-
-const buttonTel = document.querySelector('.nav-burger__icon.tel');
-const panelTel = document.querySelector('.nav-burger__panel-tel');
-
-const buttonBurger = document.querySelector('.nav-burger__icon.burger-button');
-const menuBurger = document.querySelector('.nav-burger__menu');
-
-const allPanel = document.querySelectorAll('.js-panel')
-function appearance (activeButton, activeMenu) {
-	activeButton.addEventListener('click', (event) => {
-		//const allButton = document.querySelectorAll('.nav-burger__icon');
-		
-		allPanel.forEach(panel => {
-			if (event.currentTarget.nextElementSibling !== panel) {
-				panel.classList.remove('js-appearance');
-			}
+		allPanel.forEach((panel) => {
+			panel.style.visibility = 'hidden';
 		});
-		activeMenu.classList.toggle('js-appearance');
+
+		hidePanel(event);
 	});
+
+	tab.addEventListener('mouseleave', hideServices);
 };
-
-
-appearance(buttonMap, panelMap);
-appearance(buttonTel, panelTel);
-appearance(buttonBurger, menuBurger);
